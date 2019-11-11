891af52b23803734f05035a0973c58855b2aeae7
aprilandjan <maybreezes1988@gmail.com>
2019-11-11 23:45:06 +0800

    add util method to get parent process id

--- test-process.js
+++ test-process.js
@@ -1,8 +1,10 @@
 const { execSync } = require('child_process');
-const showProcessInfo = require('./show-process-info');
+const { getCurrentProcessInfo, getParentPidByPid } = require('./utils');
 
 console.log('---> this is parent process');
-showProcessInfo();
-const info = execSync('node ./tested-child.js');
-console.log(info.toString());
+// getCurrentProcessInfo();
+// const info = execSync('node ./tested-child.js');
+// console.log(info.toString());
+const parentPid = getParentPidByPid(process.pid);
+console.log(parentPid, process.pid);
 console.log('<--- parent process finished');
---
+++ utils.js
@@ -0,0 +1,47 @@
+var execSync = require('child_process').execSync;
+
+function getProcessInfoByPid(pid) {
+  const stdout = execSync('tasklist');
+  var lines = stdout.toString().split('\n');
+  let result;
+  lines.forEach(function (line) {
+    var parts = line.split('=');
+    parts.forEach(function (items) {
+      if (items.toString().indexOf(pid) > -1) {
+        // console.log(items.toString().substring(0, items.toString().indexOf(pid)));
+        // console.log(items);
+        result = items.toString().trim();
+      }
+    })
+  });
+  return result;
+}
+
+function getCurrentProcessInfo () {
+  var selfPid = process.pid;
+  var parentPid = process.ppid;
+  // console.log('current pid', selfPid, 'parent pid', parentPid);
+  
+  //  current parent argv[0] should always be node since this program is executed by node
+  //  argv[1] is the script location
+  //  if it is started by npm or yarn, should it be npm/yarn entry script?
+  console.log(`current process: pid=[${selfPid}], execPath=[${process.argv[0]}], script=[${process.argv[1]}], taskinfo=[${getProcessInfoByPid(selfPid)}]`);
+  console.log(`parent process: pid=[${parentPid}], taskinfo=[${getProcessInfoByPid(parentPid)}]`);
+}
+
+// https://stackoverflow.com/questions/7486717/finding-parent-process-id-on-windows
+function getParentPidByPid(pid) {
+  const stdout = execSync(`wmic process get processid,parentprocessid,executablepath|find "${pid}"`);
+  // console.log(stdout.toString());
+  return stdout.toString();
+}
+
+function getAllAncesterProcessInfo (pid) {
+  // 
+}
+
+module.exports = {
+  getProcessInfoByPid,
+  getParentPidByPid,
+  getCurrentProcessInfo,
+}
