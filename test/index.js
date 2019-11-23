c40cd1666b466671e9f9fe6758e807a3b756b5b9
merlinye <merlin.ye@qq.com>
2019-11-23 20:02:15 +0800

    test: add test complete cases

--- test/index.js
+++ test/index.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const fs = require('fs');
 const exec = require('child_process').execSync;
 const assert = require('assert');
 
 const cwd = path.resolve(__dirname, './fixture/project');
-function runTest (cmd, cwd) {
+function run (cmd) {
   let success = true;
   try {
-    exec(cmd, { cwd });
+    exec(cmd, {
+      cwd,
+      env: {
+        ...process.env,
+        //  leave it undefined to prevent env inherit
+        npm_config_user_agent: undefined,
+      },
+      stdio: 'ignore',
+    });
   } catch (e) {
     success = false;
   }
@@ -14,12 +23,82 @@ function runTest (cmd, cwd) {
 }
 
 //  should correctly ensure yarn
-assert(runTest('npm run checkYarn', cwd) === false);
-assert(runTest('yarn checkYarn', cwd));
+describe('checkYarn', () => {
+  it('should correctly executed with yarn', () => {
+    assert(run('yarn checkYarn'));
+  });
+  it('should throw exception with npm', () => {
+    assert(run('npm run checkYarn') === false);
+  });
+});
 
 //  should correctly ensure npm
-assert(runTest('npm run checkNpm', cwd));
-assert(runTest('yarn checkNpm', cwd) === false);
+describe('checkNpm', () => {
+  it('should correctly executed with npm', () => {
+    assert(run('npm run checkNpm'));
+  });
+  it('should throw exception with yarn', () => {
+    assert(run('yarn checkNpm') === false);
+  });
+});
 
-//  should correctly ensure automatically
-// TODO:
+describe('check automatically', () => {
+  describe('no lock file exists', () => {
+    it('should correctly executed with npm', () => {
+      assert(run('npm run check'));
+    });
+    it('should correctly executed with yarn', () => {
+      assert(run('yarn check'));
+    });
+  });
+  describe('file yarn.lock existed only', () => {
+    const lockfile = path.join(cwd, 'yarn.lock');
+    before(() => {
+      fs.openSync(lockfile, 'w');
+    });
+    after(() => {
+      fs.unlinkSync(lockfile);
+    });
+    it('should correctly executed with yarn', () => {
+      assert(run('yarn check'));
+    });
+    it('should throw exception with npm', () => {
+      assert(run('npm run check') === false);
+    });
+  });
+
+  describe('file package-lock.json existed only', () => {
+    const lockfile = path.join(cwd, 'package-lock.json');
+    before(() => {
+      fs.openSync(lockfile, 'w');
+    });
+    after(() => {
+      fs.unlinkSync(lockfile);
+    });
+    it('should correctly executed with npm', () => {
+      assert(run('npm run check'));
+    });
+    it('should throw exception with yarn', () => {
+      assert(run('yarn check'));
+    });
+  });
+
+  describe('file yarn.lock & package-lock.json both existed', () => {
+    const npmLock = path.join(cwd, 'package-lock.json');
+    const yarnLock = path.join(cwd, 'yarn.lock');
+    before(() => {
+      fs.openSync(npmLock, 'w');
+      fs.openSync(yarnLock, 'w');
+    });
+    after(() => {
+      fs.unlinkSync(npmLock);
+      fs.unlinkSync(yarnLock);
+    });
+    it('should correctly executed with npm', () => {
+      assert(run('npm run check'));
+    });
+    it('should correctly executed with yarn', () => {
+      assert(run('yarn check'));
+    });
+  });
+});
